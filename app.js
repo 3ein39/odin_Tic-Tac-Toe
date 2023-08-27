4a11c81d97536a6c47ff86ea12d001d6bfdd2660
HusseinHany <hhany3997@gmail.com>
2023-08-27 21:45:07 +0300

    implement gameboard render

--- app.js
+++ app.js
@@ -1,35 +1,23 @@
-// player module
+let start = document.getElementById('start');
+let reset = document.getElementById('reset');
 
-var player = (function() {
-    var player = {};
+// Gameboard module
+const Gameboard = (() => {
+    let board = ["", "", "", "", "", "", "", "", ""];
 
-    player.name = '';
-    player.symbol = '';
-
-    player.setName = function(name) {
-        this.name = name;
-    };
-
-    player.setSymbol = function(symbol) {
-        this.symbol = symbol;
-    };
-
-    player.getName = function() {
-        return this.name;
-    };
-
-    player.getSymbol = function() {
-        return this.symbol;
-    };
-
-    player.getInput = function() {
-        let input = prompt('Enter your name: ');
-        this.setName(input);
-        input = prompt('Enter your symbol: ');
-        this.setSymbol(input);
+    let render = () => {
+        for (let i = 0; i < board.length; i++) {
+            let cell = document.getElementById(`${i}`);
+            cell.innerText = board[i];
+        }
     }
 
-    return player;
+
+    return {
+        // not implemented yet
+        render,
+    }
 })();
-
-
+start.addEventListener('click', () => {
+    // Game.start();
+})
