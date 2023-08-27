e873d6ed72e431ca6cb162ac3688cc185e3d2e93
HusseinHany <hhany3997@gmail.com>
2023-08-27 15:33:01 +0300

    define player object

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('Hello World');
+// player module
+
+var player = (function() {
+    var player = {};
+
+    player.name = '';
+    player.symbol = '';
+
+    player.setName = function(name) {
+        this.name = name;
+    };
+
+    player.setSymbol = function(symbol) {
+        this.symbol = symbol;
+    };
+
+    player.getName = function() {
+        return this.name;
+    };
+
+    player.getSymbol = function() {
+        return this.symbol;
+    };
+
+    player.getInput = function() {
+        let input = prompt('Enter your name: ');
+        this.setName(input);
+        input = prompt('Enter your symbol: ');
+        this.setSymbol(input);
+    }
+
+    return player;
+})();
+
+
